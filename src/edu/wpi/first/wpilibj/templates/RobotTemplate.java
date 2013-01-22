7d3f4d881bf1ec9cec03fe0d507a74bd32cc7469
SchoolComputer <1559deviltech@gmail.com>
2013-01-22 17:33:58 -0500

    SmartDashboard Project

---
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,16 @@
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.SimpleRobot;
+
+public class RobotTemplate extends SimpleRobot {
+
+    public void autonomous() {
+        
+    }
+    public void operatorControl() {
+
+    }
+    public void test() {
+    
+    }
+}
